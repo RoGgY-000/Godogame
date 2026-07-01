44f59f315e21ff38c9db20e7a295367ab1b27742
RoGgY <roggy.work@gmail.com>
2026-07-02 01:06:37 +0400

    Scenes/Enemies/baseEnemy/add CorrectRotation

--- src/game/Scenes/Enemies/baseEnemy/BaseEnemy.cs
+++ src/game/Scenes/Enemies/baseEnemy/BaseEnemy.cs
@@ -1,8 +1,10 @@
+using System;
 using Godot;
 
 public partial class BaseEnemy : MeshInstance2D
 {
 	PathFollow2D pathFollow;
+	Label hp;
 	[Export]
 	public int Health { get; set; }
 	[Export]
@@ -11,12 +13,16 @@ public partial class BaseEnemy : MeshInstance2D
 	public override void _Ready()
 	{
 		pathFollow = GetParent<PathFollow2D>();
+		hp = GetNode<Label>("HitBox/Label");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		pathFollow.Progress += (float) delta * Speed;
+		UpdateHPBar();
+		CorrectRotation();
+		GD.Print(GlobalRotationDegrees);
 		if ( Health <= 0 )
 		{
 			Kill();
@@ -25,6 +31,24 @@ public partial class BaseEnemy : MeshInstance2D
 		{
 			GameManager.Instance.PlayerHealth -= Health;
 			Kill();
+		}
+	}
+	private void CorrectRotation()
+	{
+		if ( GlobalRotationDegrees > 90 )
+		{
+			GlobalRotationDegrees -= 180;
+		}
+		else if ( GlobalRotationDegrees < -90 )
+		{
+			GlobalRotationDegrees += 180;
+		}
+	}
+	private void UpdateHPBar ()
+	{
+		if ( hp != null )
+		{
+			hp.Text = Health.ToString();
 		}
 	}
 
