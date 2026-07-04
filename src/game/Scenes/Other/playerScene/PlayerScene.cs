3e6d9cbf8b32e4827a1bfe8c15f123c53704e3c4
Nemray <74258374+May-Be-NemRay@users.noreply.github.com>
2026-07-05 03:26:41 +0400

    Отображение хп перед лицом игрока

--- src/game/Scenes/Other/playerScene/PlayerScene.cs
+++ src/game/Scenes/Other/playerScene/PlayerScene.cs
@@ -6,15 +6,21 @@ public partial class PlayerScene : Node2D
 {
 	[Export]
 	public float cameraSpeed = 5f;
+	public bool inPauseMenu = false;
+	public Label healthLabel;
 
 	public override void _Ready()
 	{
+		healthLabel = GetNode<Label>("Camera2D/Health Label");
+		// Input.MouseMode = Input.MouseModeEnum.Confined;
 	}
 
 	
 	public override void _Process(double delta)
 	{
 		Move();
+		UpdateHealthLabel();
+		// MouseFocus();
 	}
 
     private void Move()
@@ -23,5 +29,22 @@ public partial class PlayerScene : Node2D
 			new Vector2(
 				Input.GetAxis("A", "D"),
 				Input.GetAxis("W", "S")) * cameraSpeed;
+	}
+
+	private void MouseFocus()
+	{
+		if(Input.IsActionJustPressed("PAUSE (ESC)"))
+		{
+			inPauseMenu = !inPauseMenu;
+			GD.Print("1");
+		}
+
+		if(inPauseMenu) {Input.MouseMode = Input.MouseModeEnum.Visible;}
+		else if(!inPauseMenu) {Input.MouseMode = Input.MouseModeEnum.Confined;}
+	}
+
+	private void UpdateHealthLabel()
+	{
+		healthLabel.Text = GameManager.Instance.PlayerHealth.ToString();
 	}
 }
